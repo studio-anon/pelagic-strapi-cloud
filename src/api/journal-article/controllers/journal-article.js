9a4acc3159aa286734ef7070e23b3613a1d373b0
Ta Huy <ta.huy@icloud.com>
2025-12-10 19:50:50 +1100

    feat: add pelagic content model

---
+++ src/api/journal-article/controllers/journal-article.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::journal-article.journal-article');
+
---
+++ src/api/journal-article/routes/journal-article.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::journal-article.journal-article');
+
