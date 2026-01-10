9df13bbed61798ea08a74d0b37f7a3146110ce5a
Ta Huy <ta.huy@icloud.com>
2026-01-11 10:46:50 +1100

    fix: configure CSP to allow images and media in Strapi admin panel
    - Update middlewares.js to configure strapi::security middleware
    - Add CSP directives for img-src and media-src to allow localhost:1337
    - Fixes Content Security Policy errors blocking images/videos in admin panel
    - Allows admin panel to load media from Strapi uploads directory

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,39 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'http://localhost:1337',
+            'https:',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'http://localhost:1337',
+            'https:',
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+          ],
+          'style-src': [
+            "'self'",
+            "'unsafe-inline'",
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
